15bb42fbe29443578049aeac30ae85c505d573ea
Ashraf <ashrafweheliye25@gmail.com>
2022-02-15 13:48:17 +0000

    commit

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,22 +1,17 @@
 import './Genres.css';
 // import { Link } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 import MovieCards from './MovieCards';
+import useSliding from '../hooks/useSliding';
+import useSizeElement from '../hooks/useSizeElement';
 
 const genreObj = [
-    // {
-    //     id: 0,
-    //     genre: 'Popular',
-    //     url:
-    //         'api.themoviedb.org/3/discover/movie/?certification_country=US&certification=R&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
-    // },
     {
         id: 1,
         genre: 'Action',
         url: 'api.themoviedb.org/3/discover/movie?with_genres=28&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
-        // url: 'https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
     },
     {
         id: 2,
@@ -46,72 +41,77 @@ const genreObj = [
 ];
 
 export default function Genres() {
-    const [width, setWidth] = useState(1000);
+    const [width, setWidth] = useState(0);
     const [move, setMove] = useState(0);
+    const [movement, setMovement] = useState(10);
     const [event, setEvent] = useState([
         {
             id: 1,
             translation: 0,
+            movement: 0,
             genre: 'Action',
             url: 'api.themoviedb.org/3/discover/movie?with_genres=28&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
         },
         {
             id: 2,
             translation: 0,
+            movement: 0,
             genre: 'Drama',
             url: 'api.themoviedb.org/3/discover/movie?with_genres=18&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
         },
         {
             id: 3,
             translation: 0,
+            movement: 0,
             genre: 'Crime',
             url: 'api.themoviedb.org/3/discover/movie?with_genres=80&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
         },
         {
             id: 4,
             translation: 0,
+            movement: 0,
             genre: 'Children',
             url: 'api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
         },
         {
             id: 5,
             translation: 0,
+            movement: 0,
             genre: 'Romance',
             url: 'api.themoviedb.org/3/discover/movie?with_genres=10749&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
         },
         {
             id: 6,
             translation: 0,
+            movement: 0,
             genre: 'Thriller',
             url: 'api.themoviedb.org/3/discover/movie?with_genres=53&sort_by=popularity.desc&api_key=9c9a236c211df46e640b24f29796b6c0&page=1',
         },
     ]);
-    const ref = useRef(2000);
-    console.log(event);
+    const ref = useRef(0);
+    // const componentWidth = useRef();
 
-    useEffect(() => {
-        console.log('width', ref.current.clientWidth);
-    }, [ref]);
+    // console.log(event);
+
+    useLayoutEffect(() => {
+        if (ref.current.clientWidth != width) {
+            setWidth(ref.current.clientWidth);
+            console.log('width is: ', width);
+        } else {
+            console.log('width is no changed: ', width);
+        }
+    }, [ref.current]);
     const moveRight = (e) => {
-        console.log('you just clicked', e);
-        console.log(ref.current);
-        console.log(event);
-        // setMove((prev) => prev - 70);
+        console.log('ref clientWidth:', width);
+        console.log('New Movement');
 
         setEvent((prevEvent) => {
-            console.log('prevEvent: ', prevEvent);
-
             return prevEvent.filter((event) => {
-                console.log('event.genre === e: ', event.genre === e);
-
                 if (event.genre === e) {
-                    console.log(event.translation);
-
-                    return (event.translation =
-                        event.translation - ref.current.clientWidth * 0.6);
-                    console.log(event.translation);
-
-                    // setMove(event.translation);
+                    console.log('moved by:', -width * 0.7);
+                    event.movement = event.movement - width * 0.7;
+                    console.log('movement: ', event.movement);
+                    return (event.translation = event.translation - width * 0.7);
                 } else {
                     return true;
                 }
@@ -119,19 +119,15 @@ export default function Genres() {
         });
     };
     const moveLeft = (e) => {
-        console.log('you just clicked', e);
-        console.log(ref.current);
-        console.log(event);
-        // setMove((prev) => prev - 70);
-
         setEvent((prevEvent) => {
-            console.log('prevEvent: ', prevEvent);
             return prevEvent.filter((event) => {
-                console.log('event.genre === e: ', event.genre === e);
                 if (event.genre === e) {
-                    console.log(event.translation);
-                    return (event.translation =
-                        event.translation + ref.current.clientWidth * 0.6);
+                    console.log('event.translation: ', event.translation);
+                    console.log('moved by: ', width * 0.7);
+                    event.movement = event.movement - width * 0.7;
+                    console.log('movement: ', event.movement);
+
+                    return (event.translation = event.translation + width * 0.7);
                 } else {
                     return true;
                 }
@@ -154,20 +150,25 @@ export default function Genres() {
                         className='chevron-left'
                         onClick={(e) => moveLeft(section.genre)}
                     >
-                        <i class='fa-solid fa-chevron-left'></i>
+                        <i className='fa-solid fa-chevron-left'></i>
                     </div>
                     <div
                         className='posters-section'
                         ref={ref}
                         style={{ transform: `translate(${section.translation}px)` }}
                     >
-                        <MovieCards url={section.url} genre={section.genre} />
+                        <MovieCards
+                            url={section.url}
+                            genre={section.genre}
+                            width={width}
+                            setWidth={setWidth}
+                        />
                     </div>
                     <div
                         className='chevron-right'
                         onClick={(e) => moveRight(section.genre)}
                     >
-                        <i class='fa-solid fa-chevron-right'></i>
+                        <i className='fa-solid fa-chevron-right'></i>
                     </div>
                 </div>
             </div>
